4b2c4408125ce83b284d8f4655e03900211daf9d
likithb02 <likith.biyani@accenture.com>
2025-01-22 08:16:24 +0000

    Added a comment in the code of GameLauncher.java

--- chap02/GameLauncher.java
+++ chap02/GameLauncher.java
@@ -3,6 +3,7 @@ package chap02;
 public class GameLauncher {
     public static void main (String[] args) {
         GuessGame game = new GuessGame();
+        System.out.println("NEW GAME STARTED");
         game.startGame();
     }
 }
